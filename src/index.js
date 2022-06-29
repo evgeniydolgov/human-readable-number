e4e4bd3ade2c6e759d411da05e551367afbc62b2
evgeniydolgov <amyr214519@gmail.com>
2022-06-29 16:59:08 +0300

    init: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function toReadable (number) {
-  
+    let simNum = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    let tensNum = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    let hundred = ' hundred';
+    const mod = ('' + number).split('').map(Number);
+    
+    if (number === 0) {
+        return 'zero'
+    }
+    else if (number < 20) {
+        return simNum[number];
+    }
+    else if (number < 100 && number % 10 !==0) {
+        return tensNum[mod[0]] + ' ' + simNum[mod[1]];
+    }
+    else if (number < 100 && number % 10 ===0) {
+        return tensNum[mod[0]] + '' + simNum[mod[1]];
+    }
+    else if ((number - 100 * mod[0] < 10) && mod[2] !== 0) {
+        return simNum[mod[0]] + hundred + ' ' + simNum[mod[2]];
+    }
+    else if ((number - 100 * mod[0] < 20) && mod[2] !== 0) {
+        return simNum[mod[0]] + hundred + ' ' + simNum[number - 100 * mod[0]];
+    }
+    else if ((number - 100 * mod[0] < 100) && mod[2] !== 0) {
+        return simNum[mod[0]] + hundred + ' ' + tensNum[mod[1]] + ' ' + simNum[mod[2]];
+    }
+    else if (number % 100 === 0) {
+        return simNum[mod[0]] + hundred;
+    }
+    else if (number - 100 * mod[0] < 20 && mod[1] !== 0) {
+        return simNum[mod[0]] + hundred + ' ' + simNum[number - 100 * mod[0]];
+    }
+    else {
+        return simNum[mod[0]] + hundred + ' ' + tensNum[mod[1]];
+    }
 }
